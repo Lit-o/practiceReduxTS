e014dacb84781850e02a93dba7e44b62d5225346
LitvinenkoBD <boris12lit@gmail.com>
2022-12-13 20:20:04 +0300

    added MDTP with bindActionCreators in redux-react app with TS

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,14 @@
 import { connect } from "react-redux";
 import { ICounterState } from "../reducer";
+import * as actions from "../actions";
+import { bindActionCreators } from "redux";
 
 export interface propsCounter {
-    inc: () => void;
-    dec: () => void;
-    rnd: (val:number) => void;
+    incA: () => void;
+    decA: () => void;
+    rndA: (val:number) => void;
     counter: number | null;
 }
-
-
 
 // станет пропсами для компонента
 const mstp = (state: ICounterState) => {
@@ -17,15 +17,21 @@ const mstp = (state: ICounterState) => {
     }
 }
 
+// const mdtp = (dispatch:any) => {
+//     const {incA, decA, rndA} = bindActionCreators(actions, dispatch)
+//     return {
+//         inc: () => dispatch(incA()), 
+//         dec: () => dispatch(decA()),
+//         rnd: rndA
+//     }
+// }
+
 const mdtp = (dispatch:any) => {
-    return {
-        inc: () => dispatch({type: 'INC'}),
-        dec: () => dispatch({type: 'DEC'}),
-        rnd: (val:number) => dispatch({type: 'RND', valueR: val}),
-    }
+    return bindActionCreators(actions, dispatch)
 }
 
-function Counter({inc, dec, rnd, counter}:propsCounter) {
+
+function Counter({incA, decA, rndA, counter}:propsCounter) {
     const hi:string = "Hello. I'm React 17.0.2 with TS template..."
 
     return (
@@ -33,14 +39,14 @@ function Counter({inc, dec, rnd, counter}:propsCounter) {
             <div className="appWrapper" style={{textAlign: "center", fontSize: 22}}>
                 <h1>{hi}</h1>
                 <h2 id="counter">{counter}</h2>
-                <button id='dec' onClick={dec}>DEC</button>
+                <button id='dec' onClick={decA}>DEC</button>
                 {/* onClick={() => props.changeCharSelected(el.id)} */}
                 <span> </span>
-                <button id='inc' onClick={inc}>INC</button>
+                <button id='inc' onClick={incA}>INC</button>
                 <span> </span>
                 <button id='rnd' onClick={() => {
                     const rndValue = Math.ceil(Math.random() * 10)
-                    rnd(rndValue)
+                    rndA(rndValue)
                     }}>RND</button>
             </div>
         </div>
